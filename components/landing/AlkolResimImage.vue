ccc7e00b576de0e8f067e302aa5c4a58b6f44dbb
Ali Sağlamel <alisaglamel3@gmail.com>
2024-04-11 17:37:50 +0300

    siddetalkolsayfalari

---
+++ components/landing/AlkolResimImage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="col-span-12 lg:col-span-6" v-bind="$attrs">
+    <div class="w-full">
+      <img :src="require('~/assets/img/AlkolBagimlilik/4(4).jpg')" class="mt-4 sm:-mt-4 py-28" alt="" />
+    </div>
+  </div>
+</template>
--- pages/SiddetBagimlilik.vue
+++ pages/SiddetBagimlilik.vue
@@ -1,28 +1,23 @@
 <template>
   <div class="w-full">
-    <div class="w-full py-16 flex flex-col items-center">
+    <div class="w-full flex flex-col items-center">
       <h2 data-aos="flip-down" class="text-3xl sm:text-4xl font-semibold text-center">Şiddet Bağımlılığı</h2>
     </div>
-
-    <section class="w-full my-12">
+    <section class="w-full">
       <BaseSection>
         <LandingBuyTradeImage class="sm:hidden mb-8" />
         <div data-aos="fade-right" class="col-span-12 lg:col-span-6 mt-4 xl:mt-20 space-y-6 px-4">
-          <h2 class="text-4xl font-semibold sm:pr-8 xl:pr-12">
-            Bağımlılıklar İle ilgili <br class="hidden sm:block" />
-            Amaçlarımız neler?
+          <h2 class="text-4xl font-semibold sm:pr-8 xl:pr-12"> Şiddet Bağımlılığı Nedir?
           </h2>
           <p class="paragraph">
-            amaçlar çeşitli yöntemlerle toplumda farkındalık yaratmayı, önleyici tedbirler geliştirmeyi ve bağımlılıkla
-            mücadeleyi desteklemeyi hedefleyebilir. İşte bu Amaçlar Şunlardır.
+          Şiddet, insanlık için ne yazık ki yaygın bir sorun haline gelmiştir. Fiziksel, duygusal veya sözlü olarak, şiddet insanların zarar görmesine ve acı çekmesine neden olur. Ancak şiddetin çözüm getirmediği, aksine daha fazla zarara yol açtığı bir gerçektir. <br> <br>
+          Şiddet, herkesin hakkına saygı göstermek ve barış içinde bir arada yaşamak için temel bir engeldir. Şiddetin olduğu bir toplumda, insanlar korku içinde yaşarlar ve özgürce ifade edemezler. İnsanların temel haklarına saygı göstermek, barışçıl çözümler bulmak ve empati kurmak, şiddetin yerine geçebilecek en güçlü araçlardır. <br> <br>
+          Şiddetin köklerini kurutmak için toplum olarak çaba göstermeliyiz. Eğitim ve farkındalık artırma çalışmalarıyla şiddetin nedenleri üzerinde durarak, insanların şiddete başvurmadan sorunları çözebilecekleri beceriler kazanmalarını sağlamalıyız. Ayrıca, şiddetin mağdurlarına destek olmalı, onların haklarını korumalı ve toplumda şiddetin kabul edilemez olduğu mesajını güçlendirmeliyiz. <br> <br>
+          <!-- Şiddetin yerine barışı ve hoşgörüyü tercih etmek, her birimizin sorumluluğudur. Küçük çatışmaları bile şiddet yoluyla çözmek yerine, iletişim, anlayış ve uzlaşma yoluyla çözmeliyiz. Bu, daha sağlıklı, daha mutlu ve daha barışçıl bir toplum için adım atmaktır.
+          Sonuç olarak, şiddetin hiçbir kazananı yoktur. Şiddet, sadece acıyı ve zararı artırır. Bu nedenle, hepimiz şiddeti reddetmeli ve barış içinde bir arada yaşamanın yollarını bulmalıyız. Her birimiz, küçük adımlarla bile şiddete karşı durabilir ve daha iyi bir dünya için çalışabiliriz. <br> <br> -->
           </p>
-          <p class="font-bold">Farkındalık Oluşturma</p>
-          <p class="font-bold">Önleyici Tedbirler</p>
-          <p class="font-bold">Mevcut Bağımlılıkla Mücadele Yöntemlerini Desteklemek</p>
-          <p class="font-bold">Toplumsal Katılımı Artırmak</p>
-          <p class="font-bold">Etkin Değerlendirme ve İzleme</p>
         </div>
-        <LandingBuyTradeImage data-aos="fade-left" class="hidden sm:block" />
+        <LandingSiddetImage data-aos="fade-left" class="hidden sm:block py-24" />
       </BaseSection>
     </section>
 
@@ -30,12 +25,12 @@
     <section class="bg-partner relative max-w-full sm:mx-6 my-24 shadow sm:rounded-2xl overflow-hidden">
       <div class="w-full px-6 sm:px-0 py-16 flex flex-col items-center justify-center space-y-4 text-center">
         <h3 data-aos="flip-down" class="text-2xl text-neutral-800 font-semibold">
-          Sosyal Sorumluluk İle ilgilenen kuruluşlar
+          Kadına Yönelik Şiddet Konusunda Hizmet Veren Kuruluşlar
         </h3>
-        <p data-aos="flip-down" class="paragraph">Kültürel Mirasımızı Koruyalım, Geleceğe Taşıyalım!</p>
+        <p data-aos="flip-down" class="paragraph">Kadına Şiddete Hayır!</p>
         <div data-aos="fade-up" class="flex flex-wrap items-center justify-center">
           <LandingPartnerImage
-            v-for="img in ['Greenpeace_logo.png', 'TEGV.png', 'Amnesty-International-Logo.png', 'Logo_of_UNICEF.png']"
+            v-for="img in ['TC-aile.png', 'mor-cati-vakif.png', 'sosyal-politika.png',]"
             :key="img"
             :img="img"
           />
@@ -53,17 +48,17 @@
         </div>
         <div class="col-span-12 lg:col-span-5 space-y-6 px-4 sm:px-6 mt-20">
           <h2 class="text-4xl font-semibold">
-            Kötü Alışkanlıklar <span class="text-header-gradient">Hayatımızı</span> Nasıl Etkiler
+            Saldırgan Davranışlar Hangi <span class="text-header-gradient"> Durumlarda </span> Ortaya Çıkar?
+          </h2>
+          <h2 class="font-bold">
+            Şiddet eğilimindeki artışla birlikte ortaya çıkan saldırgan davranışlar;
           </h2>
           <p class="paragraph">
-            Kötü alışkanlıkların hayatımızı olumsuz yönde etkilemesi birçok şekilde olabilir. İşte bunlardan bazıları
+           <strong class="text-2xl">!</strong> Genellikle evli kadın ve erkekler arasında görülür ve bu durum aile içi şiddetin yaygınlaşmasında büyük rol oynar. <br> <br>
+           <strong class="text-2xl">!</strong> Bireyin hayatında yakın zamanda meydana gelen büyük değişikler strese ve iç gerilime neden olabilir. Bu doğrultuda gelişen içsel baskı hissi, öfke ve dürtüsellik gibi durumlar şiddet içeren davranışların ortaya çıkma riskini artırır. <br> <br>
+           <strong class="text-2xl">!</strong> 16-25 yaş arası erkek bireylerin fazlaca bulunduğu ortamlarda saldırgan davranışların ve şiddet olaylarının gelişme riski daha yüksektir. <br> <br>
+           <strong class="text-2xl">!</strong> Bireyin ruhsal gerilimini artıran olay ve kişiler, tehdit veya baskı altında olma durumu, can güvenliğinin tehlikede olması gibi koşullar  da şiddet eğilimini artıran önemli noktalar arasında yer alır. <br> <br>
           </p>
-          <ul class="space-y-4 sm:space-y-2">
-            <LandingListItem title="Sağlık Sorunları" />
-            <LandingListItem title="Finansal Zorluklar" />
-            <LandingListItem title="Kişisel İlişkilerde Sorunlar" />
-            <LandingListItem title="Duygusal ve Psikolojik Sorunlar" />
-          </ul>
         </div>
       </BaseSection>
     </section>
@@ -74,29 +69,27 @@
         <LandingTradingToolImage class="sm:hidden" />
         <div data-aos="fade-right" class="col-span-12 lg:col-span-6 space-y-8 sm:space-y-6 px-4 sm:px-6 mt-8">
           <h2 class="text-3xl font-semibold">
-            Kötü alışkanlıklardan<span class="text-header-gradient"> Uzak </span>durmak
+            Şiddetin<span class="text-header-gradient"> Nedenleri </span>Nelerdir?
           </h2>
           <div class="space-y-2">
-            <h4 class="text-lg font-medium">Hedef Belirleme ve Motivasyon</h4>
+            <h4 class="text-lg font-medium">Psikososyal Faktörler: </h4>
             <p class="paragraph text-sm xl:text-base">
-              İlk adım olarak, kötü alışkanlıklardan kurtulma isteğinizi netleştirin ve bu konuda kendinize bir hedef
-              belirleyin. Bu hedefler sizin için anlamlı olmalı ve sizi motive etmelidir.
+            Gelişimsel Faktörler:  Gelişim döneminde şiddete tanık olan veya maruz kalan çocukların yetişkinlik döneminde şiddet uygulayan bireyler olma olasılığı çok daha yüksektir. <br>
+            Çevresel Faktörler: Yapılan araştırmalar doğrultusunda kalabalık yaşam ortamlarının şiddet eğilimini artırdığı, hava durumunun (örneğin rahatsızlık yaratacak düzeyde artan ortam sıcaklığının) şiddet üzerinde etkili olduğu söylenebilir. 
             </p>
           </div>
           <div class="space-y-2">
-            <h4 class="text-lg font-medium">Nedenleri Anlama</h4>
+            <h4 class="text-lg font-medium">Sosyoekonomik Faktörler:</h4>
             <p class="paragraph text-sm xl:text-base">
-              Kötü alışkanlıkların arkasındaki nedenleri anlamak önemlidir. Bu nedenler fiziksel, duygusal veya çevresel
-              olabilir. Kendinize neden kötü alışkanlıklara başladığınızı sorun ve bu nedenlerle başa çıkma yolları
-              arayın.
+            Bu alanda yapılmış olan niteliksel çalışmaların sonuçlarında ırk ve ekonomik eşitsizlik gibi faktörlerden bağımsız olarak ağır yoksulluk durumunun ve evlilik hayatında yaşanan sorunların şiddet ile ilişkili olduğu ortaya konmuştur. <br>
+            Aile yapısında bozulmaya neden olan sosyoekonomik faktörler de bu ailelerde yetişen çocukların saldırgan davranışlarında artışa neden olur. 
             </p>
           </div>
           <div class="space-y-2">
-            <h4 class="text-lg font-medium">Destek Arama</h4>
+            <h4 class="text-lg font-medium">Psikiyatrik Faktörler: </h4>
             <p class="paragraph text-sm xl:text-base">
-              Kötü alışkanlıklardan kurtulmak için destek almak önemlidir. Aile üyeleri, arkadaşlar, terapistler veya
-              destek grupları gibi kaynaklardan yardım isteyebilirsiniz. Bu destek aynı zamanda size motivasyon ve güç
-              verebilir.
+            Psikotik bozukluk olarak tanımlanan hastalıklardan manik tipteki bipolar bozukluk, şizofreni ve paranoid bozukluklar saldırgan davranışlarda artışa neden olabilir. Bu psikiyatrik rahatsızlığa sahip olan kişilerde hem çevreye hem kendilerine yönelik şiddet uygulama eğilimi gözlenir. <br>
+            Psikotik olmayan bozukluklardan ise travma sonrası stres bozukluğu yaşayan kişilerde; borderline, paranoid ve antisosyal kişilik bozukluklarında şiddet eğiliminde artış meydana gelir ve bu kişilerde saldırgan içerikli davranışlarla son derece sık karşılaşılır. 
             </p>
           </div>
         </div>
